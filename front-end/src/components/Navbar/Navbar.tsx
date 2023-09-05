f464723d8907ce774f591c206aa0effa6ece8df8
Perseus Prince <perseus.prince070@gmail.com>
2023-09-05 12:06:11 +0200

    commit

--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,32 @@
- import React from 'react';
- import "./Navbar.css";
+import React from 'react';
+import "./Navbar.css";
  
- const Navbar = () => {
-   return (
-     <div>Navbar</div>
-   )
- }
- 
- export default Navbar
+const Navbar = () => {
+    return (
+        <nav>
+            <a href="#" className="logo">
+                Sound <span className="red">DZign</span>
+            </a>
+
+            <ul>
+                <li>
+                    <a href="#">Course Details</a>
+                </li>
+
+                <li>
+                    <a href="#">About</a>
+                </li>
+
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+
+                <li>
+                    <a href="#">Testimonials</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
