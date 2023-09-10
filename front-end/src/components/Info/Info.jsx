f8f5eac7d8365ec1577a45e6146b2d8c3b1b352e
Perseus Prince <perseus.prince070@gmail.com>
2023-09-10 16:39:53 +0200

    info complete

--- front-end/src/components/Info/Info.jsx
+++ front-end/src/components/Info/Info.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Info.css";
+import { studentIcon, videoIcon, bgElement2 } from "../../assets";
 
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <section id="info" className="dark-gray">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="info-content">
+            <img src={studentIcon} />
+            <div className="amount">23,000+</div>
+            <div className="type">Students</div>
+          </div>
+          <div className="info-content">
+            <img src={videoIcon} />
+            <div className="amount">25 Hrs</div>
+            <div className="type">Video Content</div>
+          </div>
+        </div>
+
+        <img className="bg-element-2" src={bgElement2} alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default Info;
