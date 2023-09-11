4d6f0b6987d33a7c5eddb3d2e1bc71406b3a31ee
Perseus Prince <perseus.prince070@gmail.com>
2023-09-11 02:13:06 +0200

    footer complete

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./Footer.css";
+import { instragramLogo, twitterLogo, facebookLogo } from "../../assets";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="black">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="links">
+            <a href="#" className="logo">
+              Sound<span className="red">DZign</span>
+            </a>
+
+            <div className="social-icons">
+              <a href="#">
+                <img src={facebookLogo} alt="" />
+              </a>
+              <a href="#">
+                <img src={twitterLogo} alt="" />
+              </a>
+              <a href="#">
+                <img src={instragramLogo} alt="" />
+              </a>
+            </div>
+
+            <div className="copyright">
+              This website is designed by GTCoding Ⓒ 2022
+            </div>
+          </div>
+
+          <div className="links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="links">
+            <h3>Contact Us</h3>
+            <ul>
+              <li>
+                <a href="#">contact@email.com</a>
+              </li>
+              <li>
+                <a href="#">+1 999 9999 999</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
